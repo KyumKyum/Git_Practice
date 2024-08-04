2b8d27e9eafd31df0e1896f1f021cce3ad647007
KyumKyum <mycheesepasta@gmail.com>
2024-08-04 20:14:49 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div style={{
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center"
+    }}>
+      <p style={{
+        fontSize: 52,
+        color: "#7692cf"
+      }}>Hello World!</p>
+    </div>
+  )
+}
+
+export default App;
